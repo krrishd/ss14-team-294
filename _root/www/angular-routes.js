6370e286eed741fe79db017d2d10daf55832d2f5
Ari Falkner <1arithehun@gmail.com>
2014-02-08 12:46:23 -0800

    for james

---
+++ _root/www/angular-routes.js
@@ -0,0 +1,26 @@
+app.config([
+  '$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: './angular-templates/home.html'
+    });
+    $routeProvider.when('/register', {
+      templateUrl: './angular-templates/register.html',
+      controller: 'RegisterCtrl'
+    });
+    $routeProvider.when("/dashboard", {
+      templateUrl: "/dashboard-components/marketplace.html"
+    });
+    $routeProvider.when("/dashboard/marketplace", {
+      templateUrl: "/dashboard-components/marketplace.html"
+    });
+    $routeProvider.when("/dashboard/statistics", {
+      templateUrl: "/dashboard-components/statistics.html"
+    });
+    $routeProvider.when("/dashboard/loans", {
+      templateUrl: "/dashboard-components/loans.html"
+    });
+    return $routeProvider.when("/dashboard/quests", {
+      templateUrl: "/dashboard-components/quests.html"
+    });
+  }
+]);
